bfec0a05430b63098510b3c7473e7675f2549439
AMOO <amoosamson@gmail.com>
2023-03-06 00:10:49 +0100

    first-javascript

---
+++ assets/js/main.js
@@ -0,0 +1,122 @@
+// console.log('amooo');
+// let firstName= 'samson';
+// console.log(firstName);
+
+// // list of students 
+
+// let className= ['lekan', 'kunle', 'favour', 'omole', 'tunji', 'sanya', 'bunmi'];
+// // let className=[1,2,3,4,5,6,7] 
+
+// let los;
+ 
+
+// console.log(className)
+
+// className.length
+//  className.unshift('gun');
+//  console.log(className.splice(2,4))
+//  className.splice(1,2)
+//  className.pop() 
+// className.concat('man') 
+// className.reverse()
+// className.shift()
+// className.sort()
+
+// for(let i=0; i<className.length; i++){
+//     console.log(i)
+//     console.log(className[i])
+// }
+
+// while loop
+// let i=0;
+// while(i<className.length){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let number= Number(prompt("enter any number")) ;
+// if(number%2===0){
+//     console.log("the number you input is odd number")
+// } else {
+//     console.log("the number you input is even number")
+// }
+
+
+
+// let alphabet= prompt("enter any alphabet").toLowerCase();
+// let words= "a"|| "e" || "i" || "o" || "u"
+// if (alphabet === words) {
+//     console.log("input is vowel");
+// } else {
+//     console.log("input is consonant");
+// }
+
+
+// a weather Script 
+
+// let weather= prompt("enter weather situation").toLowerCase()
+// let situation= "rainy" 
+// let site= "sunny"
+// let done= "cold"
+
+// if (weather=== situation) {
+// console.log(`its rainy ${"use your umbrella"}`);
+// } else if (weather=== site) {
+//     console.log(`its sunny ${"light cloth"}`);
+// } else if (weather=== done) {
+//     console.log(`its cold ${"put on your cardigan"}`);
+// } else{
+//     console.log("out of scope")
+// }
+
+function addNum(num1, num2){
+     return num1 + num2;
+    }
+    console.log(addNum(5,6))
+
+    
+
+
+
+// const todos = [
+//      {
+//       id: 1,
+//      task : 'Study html and css',
+//      isComplete: true
+//      },
+//      {
+//       id: 2,
+//       task : 'Study bootstrap',
+//       isComplete: true
+//      },
+//      {
+//       id: 3,
+//       task : 'Study git and git hub',
+//       isComplete: true
+//      },
+//      {
+//      id: 4,
+//       task : 'Study javascript',
+//       isComplete: false
+//      },
+//     ]
+
+
+    // for(let sam of todos){
+    //     // console.log(sam)
+    //     console.log(sam.id);
+    // }
+
+    // const tasks = todos.map(function(todo){
+    //  return todo.task
+    //     })
+    //     console.log(tasks);
+
+
+        // const tasks = todos.map(function(man){
+        //     return man.isComplete
+        // })
+        // console.log(tasks);
+
+
